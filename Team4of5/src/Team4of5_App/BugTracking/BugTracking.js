94a876dd11059e2f4bf266dfaa124d2177958a41
Yin Deascentis <yd@Yins-MBP.fios-router.home>
2017-06-11 11:46:09 -0400

    Rebuild the react table

--- Team4of5/src/Team4of5_App/BugTracking/BugTracking.js
+++ Team4of5/src/Team4of5_App/BugTracking/BugTracking.js
@@ -2,19 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from '../Navbar/Nav.js';
 
-class BugTracking extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
-    render() {
-        return (
-          <div>
-    
-              <h1>BugTracking</h1>
-          </div>
-        )
-    }
+class BugTracking extends React.Component {
+  constructor(props){
+		super(props);
+  }
+
+
+  render() {
+
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th className="td">Name</th>
+            <th className="td">Price</th>
+			<th className="td">Category</th>
+			<th className="td">Operation</th>
+          </tr>
+        </thead>
+
+      </table>
+    );
+  }
 }
 
 export default BugTracking;
