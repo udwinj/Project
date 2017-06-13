fccc12f871d7aa34f972ec7d102832f96b964a01
Alisa Aylward <aaylward@BOS1002979l.local>
2017-06-12 21:37:11 -0400

    adding functiojn for creating new bug

---
+++ Team4of5/src/Team4of5_Service/Bugs.js
@@ -0,0 +1,25 @@
+
+
+import * as firebase from 'firebase';
+import * as Config from './Config.js';
+
+
+firebase.initializeApp(Config.firebase_config);
+var ref = firebase.app().database().ref();
+var bugRef = ref.child('bugs');
+
+export const addNewBug = function(bug_id,bug_type, bug_reporter, bug_reporter_role,bug_assignee,bug_issue_date,details,status,expcompdt){
+		var thisBugRef = bugRef.child(user_uid);
+              thisBugRef.update({
+              id: bug_id,
+              type: bug_type,
+              reporter: bug_reporter,
+              reporter_role: bug_reporter_role,
+              assignee: bug_assignee,
+              issue_date: bug_issue_date,
+              details: details,
+              status: status,
+              expected_completion_dt: expcompdt,
+              actual_completion_dt: '',
+              });
+}
