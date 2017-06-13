43ddefc6d46ea79ddfbaa7da3cb87162cf8c6fc6
Alisa Aylward <aaylward@BOS1002979l.local>
2017-06-12 21:49:47 -0400

    fjxing an error and adding a function

--- Team4of5/src/Team4of5_Service/Bugs.js
+++ Team4of5/src/Team4of5_Service/Bugs.js
@@ -9,7 +9,7 @@ var ref = firebase.app().database().ref();
 var bugRef = ref.child('bugs');
 
 export const addNewBug = function(bug_id,bug_type, bug_reporter, bug_reporter_role,bug_assignee,bug_issue_date,details,status,expcompdt){
-		var thisBugRef = bugRef.child(user_uid);
+		var thisBugRef = bugRef.child(bug_id);
               thisBugRef.update({
               id: bug_id,
               type: bug_type,
@@ -21,5 +21,12 @@ export const addNewBug = function(bug_id,bug_type, bug_reporter, bug_reporter_ro
               status: status,
               expected_completion_dt: expcompdt,
               actual_completion_dt: '',
+              });
+}
+
+export const updateBugStatus = function(bug_id,status){
+              var thisBugRef = bugRef.child(bug_id);
+              thisBugRef.update({
+              status: status
               });
 }
