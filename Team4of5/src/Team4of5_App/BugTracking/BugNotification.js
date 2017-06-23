86bc73d5c7744f06124634d0fa0f544a30d0b054
Alisa Aylward <aaylward@dhcp-wifi-8021x-155-41-53-15.bu.edu>
2017-06-22 20:40:15 -0400

    prompt from unread bugs

--- Team4of5/src/Team4of5_App/BugTracking/BugNotification.js
+++ Team4of5/src/Team4of5_App/BugTracking/BugNotification.js
@@ -1,25 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import * as firebase from 'firebase';
+import * as Config from '../../Team4of5_Service/Config.js';
 
 
-const bugNotificationTypes = [{
-  id: "1",
-  name: "Unread Bugs",
-  number: 20
-},
-{
-  id:"2",
-  name: "Imcomplete Bugs",
-  number: 8
-},
-];
 
 export default class BugNotification extends React.Component {
+  constructor(props) {
+        super(props);
+
+     this.state = {
+      bugInfo:[],
+      bugNotificationTypes: [{
+            id: "1",
+            name: "Unread Bugs",
+            number: 9
+          },
+          {
+            id:"2",
+            name: "Incomplete Bugs",
+            number: 9
+          },
+          ]
+    };
+    this.bugRef = firebase.database().ref().child('bugs');
+  }
+    componentDidMount() {
+
+          this.bugRef.on('value', this.gotData, this.errData);
+        }
+
+    gotData = (data) => {
+      var unread = 0;
+      var incomplete = 0;
+      let newBug = []
+      const bugdata = data.val();
+      const keys = Object.keys(bugdata);
+      for (let i = 0; i < keys.length; i++) {
+      const k = keys[i];
+      if (bugdata[k].Status == "Unread"){
+            unread = unread+1;
+        }
+      else if (bugdata[k].Status == "Incomplete") {
+            incomplete = incomplete +1;
+      }
+      }
+      newBug.push(unread);
+      newBug.push(incomplete);
+      this.setState({bugInfo: newBug});
+      alert("there are " + this.state.bugInfo[0] + " unread bugs")
+
+
+  }
+
   render() {
     return (
       <BootstrapTable
-      data={ bugNotificationTypes }
+      data={ this.state.bugNotificationTypes }
       exportCSV = {false}
       striped={ true }
       tableHeaderClass='flakes-table'
