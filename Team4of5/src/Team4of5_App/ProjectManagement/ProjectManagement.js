2c0cbb789e70ba007d7efdfb836364d2a4271a73
Alisa Aylward <aaylward@chewy.com>
2017-06-29 10:45:59 -0400

    adding title to project management page

--- Team4of5/src/Team4of5_App/ProjectManagement/ProjectManagement.js
+++ Team4of5/src/Team4of5_App/ProjectManagement/ProjectManagement.js
@@ -3,16 +3,43 @@ import ReactDOM from 'react-dom';
 import Navbar from '../Navbar/Nav.js';
 import ProjectManagementTable from './ProjectManagementTable.js';
 import './ProjectManagementTables.css';
+import * as Users from '../../Team4of5_Service/Users.js';
 
 class ProjectManagement extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            userInfo: [],
+        }
+        this.getData = this.getData.bind(this);
+
     }
+    componentDidMount() {
+          let self = this;
+          if(Users.userExist() == true){
+            Users.getUserData().then(function (data) {
+                      self.getData(data);
+                    }, function (err) {
+                        //Error occur
+                        console.log("Promise Error");
+                        console.log(err);
+                    })
+          }
+          }
+
+        getData(data) {
+                let newUser = []
+                const userdata = data.val();
+                //const keys = Object.keys(userdata);
+
+                newUser.push(userdata.role);
+                this.setState({ userInfo: newUser });
+            }
 
     render() {
         return (
         <div>
-            <h1>Project Management</h1>
+            <h1>Project Management {this.state.userInfo[0]} Page</h1>
           <div id="parent">
 
             <br/>
