1a38b5707c80fc5db769b3316a72be51695c01e9
xyapple <ydeascentis@gmail.com>
2017-06-29 14:12:51 -0400

    Add a navbars for the create issue page

--- Team4of5/src/Team4of5_App/BugTracking/NewIssue.js
+++ Team4of5/src/Team4of5_App/BugTracking/NewIssue.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from '../../registerServiceWorker.js';
-import { Navbar, Jumbotron, Button, Input, Nav } from 'react-bootstrap';
+import Navbar from '../Navbar/Nav.js';
+import IssueTrackerNav from './IssueTrackerNav.js';
 
 //Connect Firebase
 import * as Config from '../../Team4of5_Service/Config.js';
@@ -41,7 +42,7 @@ class NewIssue extends React.Component {
      if (this.state.completionDate
          &&this.state.details&&this.state.expComDate
          &&this.state.issueDate&&this.state.owner
-         &&this.state.project&&this.state.status) 
+         &&this.state.project&&this.state.status)
      {
              Issues.addNewIssue(
                   this.state.completionDate,this.state.details,
@@ -57,7 +58,7 @@ class NewIssue extends React.Component {
             alert("Please fill all fields");
           }
 
-        
+
   event.preventDefault();
 }
 
@@ -71,6 +72,8 @@ class NewIssue extends React.Component {
            )
          }
     return (
+        <div>
+        <IssueTrackerNav />
       <form className='createIssue' onSubmit={this.handleSubmit}>
           <div className="title">
               <h1>Create New Issue</h1>
@@ -122,6 +125,7 @@ class NewIssue extends React.Component {
 
         <input type="submit" id="submitBtn" value={this.state.formBtnTxt} />
       </form>
+      </div>
     );
   }
 
