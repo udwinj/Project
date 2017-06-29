0545c35d43c2af93509d9a5e5d0d273ca1005467
xyapple <ydeascentis@gmail.com>
2017-06-29 12:02:18 -0400

    Still working on the IssueTrackerBody

---
+++ Team4of5/src/Team4of5_Service/Issues.js
@@ -0,0 +1,52 @@
+
+
+import * as firebase from 'firebase';
+import * as Config from './Config.js';
+
+
+var ref = firebase.app().database().ref();
+var issueRef = ref.child('issues');
+
+
+export const addNewIssue = function(
+    issue_id, completionDate, details,
+    expComDate, issueDate, owner, project,
+       issue_status){
+        var thisIssueRef = issueRef.child(issue_id);
+        
+	    thisIssueRef.update({
+              id: issue_id,
+              owner: owner,
+              completionDate: completionDate,
+              details: details,
+              expComDate: expComDate,
+              issuedate: issueDate,
+              project: project,
+              status: issue_status,
+              issue_last_edited_dtm: Date.now()
+              });
+}
+
+export const updateIssueStatus = function(issue_id,status){
+              var thisIssueRef = issueRef.child(issue_id);
+              thisIssueRef.update({
+              status: status,
+              issue_last_edited_dtm: Date.now()
+              });
+}
+
+export const updateOwner = function(issue_id,owner){
+              var thisIssueRef = issueRef.child(issue_id);
+              thisIssueRef.update({
+              owner: owner,
+              issue_last_edited_dtm: Date.now()
+              });
+}
+
+export const update_completion_dt = function(issue_id,completionDate){
+              var thisIssueRef = issueRef.child(issue_id);
+              thisIssueRef.update({
+              completiondate: completionDate,
+              bug_last_edited_dtm: Date.now()
+              });
+}
