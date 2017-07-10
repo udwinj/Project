c0f241f17d2cf42b05dcc52848e05798b25abc2d
kjmyers <kevin.j.myers@gmail.com>
2017-07-10 19:36:29 -0400

    Changing getdata function

--- Team4of5/src/Team4of5_App/ProjectManagement/ProjectManagement.js
+++ Team4of5/src/Team4of5_App/ProjectManagement/ProjectManagement.js
@@ -17,7 +17,8 @@ import * as Issues from '../../Team4of5_Service/Issues.js';
 const issueData = []
 
 
-const data = {
+
+/*var data = {
     lanes: [
         {
             id: 'Backlog',
@@ -80,7 +81,8 @@ const data = {
             { id: 'Card2', title: 'Card2', description: 'As a user, I can set myself as away to snooze notifications related to chat.', tags: [{ title: 'Chat', color: 'black', bgcolor: 'yellow' }] }]
         }
     ]
-}
+}*/
+
 
 
 let eventBus = undefined
@@ -94,8 +96,8 @@ const completeMilkEvent = () => {
     eventBus.publish({ type: 'REMOVE_CARD', laneId: 'PLANNED', cardId: "Milk" })
 }
 
-const addBlockedEvent = () => {
-    eventBus.publish({ type: 'ADD_CARD', laneId: 'lane4', card: { id: "Ec2Error", title: "EC2 Instance Down", label: "30 mins", description: "Main EC2 instance down" } })
+const addCard = (laneID, id, title, description) => {
+    eventBus.publish({ type: 'ADD_CARD', laneId: laneID, card: { id: id, title: title, label: "30 mins", description: description } });
 }
 
 const handleDragStart = (cardId, laneId) => {
@@ -116,69 +118,105 @@ const shouldReceiveNewData = (nextData) => {
     console.log(nextData)
 }
 
-function GetCardInfo(props) {
 
-    return (
-        <div className="warning">
-            This Card is {props.warn}
 
-            <form className="pure-form pure-form-aligned">
-                <fieldset>
-                    <div className="pure-control-group">
-                        <label for="descrip">Description:</label>
-                        <input id="descrip" type="text" placeholder="Username" />
-                    </div>
-                    <div className="pure-control-group">
-                        <label for="label"> Label: </label>
-                        <select id="label">
-                            <option selected value="login">Login & User Roles</option>
-                            <option value="chat">Chat</option>
-                            <option value="homepage">Homepage</option>
-                            <option value="defect">Defect</option>
-                            <option value="kanban">Kanban Board</option>
-                            <option value="issue">Issue Tracking</option>
-                            <option value="multitenant">Multi-Tenant Management</option>
-                        </select>
-                    </div>
-                    <div className="pure-control-group">
-                        <label for="assign">Assignment:</label>
-                        <select id="assign">
-                            <option selected value="kevin">Kevin</option>
-                            <option value="alisa">Alisa</option>
-                            <option value="kyle">Kyle</option>
-                            <option value="yin">Yin</option>
-                            <option value="joel">Joel</option>
-                        </select>
 
-                    </div>
-                    <div className="pure-control-group">
-                        <label for="stage">Stage:</label>
-                        <select id="stage">
-                            <option value="backlog">Backlog</option>
-                            <option value="next">Next</option>
-                            <option selected value="inProgress">In Progress</option>
-                            <option value="staged">staged</option>
-                            <option value="qa">QA</option>
-                            <option value="live">Live</option>
-                        </select>
-                    </div>
-                    <div className="pure-controls">
 
-                        <input className="pure-button pure-button-primary" type="submit" value="Submit" />
-                    </div>
-                    
-                </fieldset>
-            </form>
-        </div>
-    );
+
+
+
+class GetCardInfo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            description: '',
+            label: '',
+            assignment: '',
+            stage: 'Backlog'
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.handleSaveModal(this.state.stage, 5, "title", this.state.description);
+    }
+
+    render() {
+        return (
+            <div className="warning">
+
+                <form className="pure-form pure-form-aligned" onSubmit={this.handleSubmit}>
+                    <fieldset>
+                        <div className="pure-control-group">
+                            <label for="descrip">Description:</label>
+                            <textarea id="descrip" name="description" rows="5" cols="50" placeholder="Description" onChange={this.handleChange}>
+
+                            </textarea>
+                        </div>
+                        <div className="pure-control-group">
+                            <label for="label"> Label: </label>
+                            <select id="label" name="label" onChange={this.handleChange}>
+                                <option value="login">Login & User Roles</option>
+                                <option value="chat">Chat</option>
+                                <option value="homepage">Homepage</option>
+                                <option value="defect">Defect</option>
+                                <option value="kanban">Kanban Board</option>
+                                <option value="issue">Issue Tracking</option>
+                                <option value="multitenant">Multi-Tenant Management</option>
+                            </select>
+                        </div>
+                        <div className="pure-control-group">
+                            <label for="assign">Assignment:</label>
+                            <select id="assign" name="assignment" onChange={this.handleChange}>
+                                <option value="kevin">Kevin</option>
+                                <option value="alisa">Alisa</option>
+                                <option value="kyle">Kyle</option>
+                                <option value="yin">Yin</option>
+                                <option value="joel">Joel</option>
+                            </select>
+
+                        </div>
+                        <div className="pure-control-group">
+                            <label for="stage">Stage:</label>
+                            <select id="stage" name="stage" onChange={this.handleChange}>
+                                <option value="Backlog">Backlog</option>
+                                <option value="Next">Next</option>
+                                <option value="InProgress">In Progress</option>
+                                <option value="Staged">Staged</option>
+                                <option value="QA">QA</option>
+                                <option value="Live">Live</option>
+                            </select>
+                        </div>
+                        <div className="pure-controls">
+                            <input className="pure-button pure-button-primary" type="submit" value="Submit" />
+                        </div>
+
+                    </fieldset>
+                </form>
+            </div>
+        );
+    }
 }
-function MakeNewCard(props) {
 
-    return (
-        <div className="warning">
-            This Card is {props.warn}
-        </div>
-    );
+
+
+
+
+function MakeNewCard(props) {
+    addCard;
 }
 
 class ProjectManagement extends React.Component {
@@ -187,56 +225,46 @@ class ProjectManagement extends React.Component {
         super();
         this.state = {
             showModal: false,
-            modalID: null
+            modalID: null,
+            lanes: []
         };
 
         this.handleOpenModal = this.handleOpenModal.bind(this);
         this.handleCloseModal = this.handleCloseModal.bind(this);
+        this.handleSaveModal = this.handleSaveModal.bind(this);
 
-        this.issueRef = firebase.database().ref().child('project');
+        this.issueRef = firebase.database().ref().child('chatProject/0be3f584-33ce-11e2-7b8c-72fe4d59dc4f/data');
+        //
 
     }
     //After the connect, what the state will do--gotdata
     componentDidMount() {
-        this.issueRef.on('value', this.gotData, this.errData);
-
+        this.issueRef.on('value', this.getData, this.errData);
     }
 
     //get the data from the firebase and push them out
-    gotData = (fire_data) => {
+    getData = (fire_data) => {
         let newIssue = []
         const issuedata = fire_data.val();
         const keys = Object.keys(issuedata);
-        /*
-                for (let i = 0; i < keys.length; i++) {
-                    const k = keys[i];
-        
-                    newIssue.push({
-                        id: k, status: issuedata[k].status,
-                        issueDate: issuedate_reformat,
-                        owner: issuedata[k].owner,
-                        expComDate: issuedata[k].expComDate,
-                        details: issuedata[k].details,
-                        completionDate: completionDate_reformat,
-                        project: issuedata[k].project
-                    });
-        
-                    {
-                        
-                    id: 'Staged',
-                    title: 'Staged',
-                    label: '0/0',
-                    cards: [{
-                        id: 'Card1', title: 'Card1', description: 'As an administrator, I can login, add projects, & manage other users.',
-                        tags: [{ title: 'Login and User Roles', color: 'white', bgcolor: 'green' }]
-                    },
-                    { id: 'Card2', title: 'Card2', description: 'As a user, I can set myself as away to snooze notifications related to chat.', tags: [{ title: 'Chat', color: 'black', bgcolor: 'yellow' }] }]
-                },
-                
-        
-                }
-                this.setState({ issues: newIssue });
-                */
+
+        for (let i = 0; i < keys.length; i++) {
+            const k = keys[i];
+
+            /*newIssue.push({
+                id: k, status: issuedata[k].status,
+                issueDate: issuedate_reformat,
+                owner: issuedata[k].owner,
+                expComDate: issuedata[k].expComDate,
+                details: issuedata[k].details,
+                completionDate: completionDate_reformat,
+                project: issuedata[k].project
+            });*/
+            newIssue.push({ lanes: issuedata[k] })
+
+        }
+        this.setState({lanes: newIssue});
+
     }
 
 
@@ -255,13 +283,23 @@ class ProjectManagement extends React.Component {
         this.setState({ showModal: false });
     }
 
+    handleSaveModal(laneId, id, title, description) {
+        addCard(laneId, id, title, description);
+        this.setState({ showModal: false });
+    }
+
+    handleCelsiusChange(temperature) {
+        this.setState({ scale: 'c', temperature });
+    }
+
+    //<button onClick={completeMilkEvent} style={{ margin: 5 }}>Complete Buy Milk</button>
     render() {
         return (
             <div>
-                <button onClick={completeMilkEvent} style={{ margin: 5 }}>Complete Buy Milk</button>
-                <button onClick={() => { this.handleOpenModal(0) }} style={{ margin: 5 }}>Add Blocked</button>
+
+                <button onClick={() => { this.handleOpenModal(0) }} style={{ margin: 5 }}>Add New Card</button>
                 <Board
-                    data={data}
+                    data={this.state.lanes}
                     draggable={true}
                     onDataChange={shouldReceiveNewData}
                     handleDragStart={handleDragStart}
@@ -276,9 +314,8 @@ class ProjectManagement extends React.Component {
                     className="Modal"
                     overlayClassName="Overlay"
                 >
-                    <GetCardInfo warn={this.state.modalID} />
-                    <button onClick={this.handleCloseModal}>Save</button>
-                    <button onClick={this.handleCloseModal}>Cancel</button>
+                    <GetCardInfo warn={this.state.modalID} handleSaveModal={this.handleSaveModal} />
+                    <button className="pure-button pure-button-primary" onClick={this.handleCloseModal}>Cancel</button>
                 </ReactModal>
             </div>);
     }
