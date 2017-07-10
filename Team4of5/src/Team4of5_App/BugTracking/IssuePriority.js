54f5cafe9f798203643d91b868a15e89dbcd6e58
xyapple <ydeascentis@gmail.com>
2017-07-10 12:11:42 -0400

    Add Issue priority table and issue serverity table

---
+++ Team4of5/src/Team4of5_App/BugTracking/IssuePriority.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+const IssuePriorityData=[
+    {
+        id:1,
+        name:"Immediate",
+        type:  "All work stops except for this item, we release the fix as soon as it is tested.",
+    },
+    {
+        id:2,
+        name:"High",
+        type: "The next release will not go out without this item resolved.",
+    },
+    {
+        id:3,
+        name:"Medium",
+        type: "Really desired in this release, but if we run out of time we will push it.",
+    },
+    {
+        id:4,
+        name:"Low",
+        type: "We really don't expect to get to this in this release, but if you run out of tasks, work on it.",
+    },
+    {
+        id:5,
+        name:"Fix if time",
+        type: " Don't work on it now.",
+    },
+]
+
+export default class IssuePriority extends React.Component {
+
+  render() {
+    return (
+      <BootstrapTable
+      data={ IssuePriorityData}
+      exportCSV = {false}
+      striped={ true }
+      hover={ true }
+      >
+        <TableHeaderColumn isKey={true} dataField='id' hidden={true}>ID</TableHeaderColumn>
+        <TableHeaderColumn dataField='name' tdStyle={ { whiteSpace: 'normal' } ,{width:150}}>Issue Priority</TableHeaderColumn>
+        <TableHeaderColumn dataField='type' tdStyle={ { whiteSpace: 'normal' } }>Details</TableHeaderColumn>
+
+      </BootstrapTable>
+    );
+  }
+}
