8a1ab1e4e9ca1636fc34e9c0ebe2f8430809fc40
kjmyers <kevin.j.myers@gmail.com>
2017-07-12 15:34:42 -0400

    Delete function working as well as other small things

--- Team4of5/src/Team4of5_Service/ChatProject.js
+++ Team4of5/src/Team4of5_Service/ChatProject.js
@@ -78,3 +78,82 @@ export const addNewCard = function (laneID, id, title, description) {
         }
     );
 }
+
+
+export const removeCard = function (laneID, id, title, description) {
+
+    var card_lane;
+    switch (laneID) {
+        case "Backlog":
+            card_lane = 0;
+            break;
+        case "Next":
+            card_lane = 1;
+            break;
+        case "InProgress":
+            card_lane = 2;
+            break;
+        case "Staged":
+            card_lane = 3;
+            break;
+        case "QA":
+            card_lane = 4;
+            break;
+        case "Live":
+            card_lane = 5;
+            break;
+        default:
+            card_lane = 0;
+    }
+    getProjectData().then(function (data) {
+
+        const projdata = data.val();
+        var projArray = [];
+        var lanes = [];
+        var cards = [];
+        const keys = Object.keys(projdata);
+        projArray.push(projdata[keys].data.lanes);
+        lanes = projArray[0];
+        //cards = lanes[card_lane].cards;
+        //cards.push({ id: id, title: title, description: description });
+
+        
+        for(let i=0; i < lanes[card_lane].cards.length; i++) {
+            cards[i] = lanes[card_lane].cards[i];
+        }
+        
+        //var array = [2, 5, 9];
+        //var index = cards.indexOf({id:id});
+        var index = cards.findIndex(x => x.id==id);
+
+        console.log("Next is index");
+        console.log(index);
+        console.log("Next is cards");
+        console.log(cards.length);
+
+        if (index > -1) {
+            cards.splice(index, 1);
+        }
+
+        //chatProject.push(lanes);
+
+        var path = '0be3f584-33ce-11e2-7b8c-72fe4d59dc4f/data/lanes/' + card_lane;
+
+        var thisRef = chatProject.child(path);
+        console.log("Checking");
+        console.log(thisRef);
+
+        //f (user_display_name && user_role) {
+        return thisRef.update({
+            cards: cards
+        });
+        //}
+
+    },
+        function (err) {
+            //Error occur
+            console.log("Promise Error");
+            console.log(err);
+        }
+    );
+}
