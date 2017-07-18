ab3704eb6915b9399feebdd53441cc7450d89659
xyapple <ydeascentis@gmail.com>
2017-07-17 20:11:32 -0400

    Update the Issue Summary navigation bar

--- Team4of5/src/Team4of5_App/BugTracking/IssueTrackerNav.js
+++ Team4of5/src/Team4of5_App/BugTracking/IssueTrackerNav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar, NavItem, Nav, Jumbotron, Button, Input} from 'react-bootstrap';
-
+import { LinkContainer } from 'react-router-bootstrap';
 import {
     BrowserRouter as Router,
     Route,
@@ -25,21 +25,27 @@ render(){
 
   return (
       <div>
-         <Navbar>
-        <Nav >
-          <NavItem>
-            <NavLink activeClassName='active' to='/menu/IssueTracker'>Issue</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink activeClassName='active' to='/menu/IssueReports'>Reports</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink activeClassName='active' to='/menu/IssueWiki'>Issue Wiki</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink activeClassName='active' to='/menu/NewIssue'>Create New Issue</NavLink>
-          </NavItem>
+         <Navbar  activeKey="1" >
+        <Nav bsStyle="pills">
+            <LinkContainer  to='/menu/IssueTracker'>
+                  <NavItem  eventKey={1}>Issue</NavItem>
+              </LinkContainer>
 
+
+                <LinkContainer  to='/menu/IssueReports'>
+                  <NavItem  eventKey={2}>Reports</NavItem>
+              </LinkContainer>
+
+
+                  <LinkContainer  to='/menu/NewIssue'>
+                  <NavItem  eventKey={4}>Create New Issue</NavItem>
+              </LinkContainer>
+
+        </Nav>
+        <Nav pullRight bsStyle="pills">
+            <LinkContainer  to="/menu/IssueWiki">
+                  <NavItem  eventKey={3}>Issue Wiki</NavItem>
+            </LinkContainer>
         </Nav>
         </Navbar>
     </div>
